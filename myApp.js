0a870df4d7a3ad9b2c3416a4e0f857edef186df4
IRUMVA DANNY <69241853+DannyIRUMVA@users.noreply.github.com>
2023-09-18 10:40:39 +0200

    Updated

--- myApp.js
+++ myApp.js
@@ -1,39 +1,23 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const app = express();
 
+// Load environment variables from .env
+require('dotenv').config();
 
+app.get('/json', (req, res) => {
+  // Check the value of MESSAGE_STYLE
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    // If it's "uppercase," transform the message to uppercase
+    res.json({ message: 'HELLO JSON' });
+  } else {
+    // Otherwise, return the default message
+    res.json({ message: 'Hello json' });
+  }
+});
 
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
