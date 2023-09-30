7bef906d66eafbdf3496beb6d1b933050538b22c
dannyirumva <boyg87059@gmail.com>
2023-09-30 09:34:08 +0200

    Updated

--- myApp.js
+++ myApp.js
@@ -1,23 +1,21 @@
+// Import the necessary packages
 const express = require('express');
+require('dotenv').config(); // Load environment variables from .env
+
+// Create an instance of Express
 const app = express();
 
-// Load environment variables from .env
-require('dotenv').config();
+// Your routes and other middleware go here
 
+// Define the /json GET route handler
 app.get('/json', (req, res) => {
-  // Check the value of MESSAGE_STYLE
-  if (process.env.MESSAGE_STYLE === 'uppercase') {
-    // If it's "uppercase," transform the message to uppercase
-    res.json({ message: 'HELLO JSON' });
-  } else {
-    // Otherwise, return the default message
-    res.json({ message: 'Hello json' });
-  }
+  // Access the MESSAGE_STYLE environment variable and transform the response message
+  const messageStyle = process.env.MESSAGE_STYLE || 'uppercase'; // Default to 'uppercase' if not set
+  const message = messageStyle === 'uppercase' ? 'HELLO JSON' : 'Hello json';
+  res.json({ message });
 });
 
 // Start the server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
 });
-
